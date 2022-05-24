39d576f765139ad12c7625259ce766ff820c8749
1ri5 <iriscyst@gmail.com>
2022-05-24 17:24:40 +0200

    Testing Components

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+export default function Navbar () {
+    return (
+    <div>
+        <img src="" />
+        <h1>Soundwave</h1>
+        <ul>
+            <li><h2>Discover</h2></li>
+            <li><h2>Join</h2></li>
+        </ul>
+    </div>
+    )
+}
