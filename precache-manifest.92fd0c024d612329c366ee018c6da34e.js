e1a9b99330ae0213b3f781b3741df9dc232af4dc
1ri5 <iriscyst@gmail.com>
2022-05-30 19:32:30 +0200

    Updates

---
+++ precache-manifest.92fd0c024d612329c366ee018c6da34e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd667b1dcaa79db214aa",
+    "url": "/SoundWave/static/css/main.ec3d3de4.chunk.css"
+  },
+  {
+    "revision": "bd667b1dcaa79db214aa",
+    "url": "/SoundWave/static/js/main.bd667b1d.chunk.js"
+  },
+  {
+    "revision": "c8ff8a031fedc5f83016",
+    "url": "/SoundWave/static/js/1.c8ff8a03.chunk.js"
+  },
+  {
+    "revision": "91c2b01d2913c4d79f3c",
+    "url": "/SoundWave/static/js/2.91c2b01d.chunk.js"
+  },
+  {
+    "revision": "3e65800507d04718f842",
+    "url": "/SoundWave/static/js/runtime~main.3e658005.js"
+  },
+  {
+    "revision": "9c7d57b29609b9e0e42a5d86850c5c50",
+    "url": "/SoundWave/static/media/twitter.9c7d57b2.svg"
+  },
+  {
+    "revision": "eea1504d2f93b9d1f75c3a9d9e488fc0",
+    "url": "/SoundWave/static/media/facebook.eea1504d.svg"
+  },
+  {
+    "revision": "d92486b09c45b7be4c5338636c4d3837",
+    "url": "/SoundWave/static/media/landing-page-girl.d92486b0.png"
+  },
+  {
+    "revision": "db4826b38ae999077bfe926f19825f87",
+    "url": "/SoundWave/static/media/background1.db4826b3.png"
+  },
+  {
+    "revision": "b42b112de2ea0a0346949a2e7dd8930e",
+    "url": "/SoundWave/static/media/covers.b42b112d.jpg"
+  },
+  {
+    "revision": "b717fa9b94edc5cb31d0ff8de7e86e5d",
+    "url": "/SoundWave/static/media/microphone.b717fa9b.svg"
+  },
+  {
+    "revision": "fb89e7109aea01527178ee4b4c190184",
+    "url": "/SoundWave/static/media/albums.fb89e710.svg"
+  },
+  {
+    "revision": "5f583ced04baae96e746f5e605b92379",
+    "url": "/SoundWave/static/media/more.5f583ced.svg"
+  },
+  {
+    "revision": "35ec2976cf6b27eb964481697784a052",
+    "url": "/SoundWave/index.html"
+  }
+];
